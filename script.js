50ffcf2e8ee08dd0934522caf7f56db5bf1de148
paulDev <Paulanselem35@gmail.com>
2025-02-20 20:52:18 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navItems = document.querySelector(".nav-items");
+const mainContent = document.querySelector(".main-content");
+
+navToggle.addEventListener("click", () => {
+  navItems.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+  mainContent.classList.toggle("blur");
+});
+
+const observerOptions = {
+  threshold: 0.1,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+document
+  .querySelectorAll(
+    ".fade-in, .fade-top, .fade-bottom, .text, .animate-img, .sect2-text-animate"
+  )
+  .forEach((section) => {
+    observer.observe(section);
+  });
+
+document.querySelector(".send-btn input").addEventListener("click", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const phone = document.getElementById("telephone").value.trim();
+  const subject = document.getElementById("subject").value.trim();
+  const message = document.getElementById("text").value.trim();
+
+  if (!name || !email || !phone || !subject || !message) {
+    alert("All fields are required.");
+    return;
+  }
+
+  if (!validateEmail(email)) {
+    alert("Please enter a valid email address.");
+    return;
+  }
+
+  alert("Form submitted successfully!");
+  // Optionally, you can submit the form here
+});
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(String(email).toLowerCase());
+}
